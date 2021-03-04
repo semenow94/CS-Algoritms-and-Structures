81bf4d20d540d22dfe68c523d40474431b2d0ebb
Semenow94 <semenow94@gmail.com>
2021-03-04 17:26:11 +0300

    HW3

---
+++ HW3-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//пока не готово, до выходных к сожалению
+namespace HW3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
